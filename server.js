819b139668f4741945fe40e6400cb887b958be9d
sonu2819 <bsonu2819@gmail.com>
2025-08-31 14:42:42 +0530

    update: improved chat UI with left-right messages

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public")); // serve frontend files
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("sendMessage", (msg) => {
-    io.emit("receiveMessage", { id: socket.id, text: msg });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
-server.listen(process.env.PORT || 5000, () => {
-  console.log("Server running...");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
